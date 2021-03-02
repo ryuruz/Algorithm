66a0ed3d4d8bea0e54116e018b13c45a649e0a13
ryuruz <77661701+ryuruz@users.noreply.github.com>
2021-03-02 13:40:36 +0900

    Create a4-1.py

---
+++ Python Algorithm Interview/6. String Manipulation/a4-1.py
@@ -0,0 +1,8 @@
+import collections
+
+def mostCommonWord(self, paragraph: str, banned: List[str]):
+  words = [word for word in re.sub(r'[^\W]', '', paragraph).lower().split()
+            if word not in banned]
+  counts = collections.Counter(words)
+  
+  return counts.most_common(1)[0][0]
